25bb673b57dd9d060f970fc62f33e0d90400380c
Utkarsh <utkarsh.dwivedi@jci.com>
2013-03-26 14:26:23 +0530

    checkedin

--- BrainMess/BrainMessUnitTestProject/UnitTest1.cs
+++ BrainMess/BrainMessUnitTestProject/UnitTest1.cs
@@ -12,26 +12,46 @@ namespace BrainMessUnitTestProject
         [TestMethod]
         public void TestMethodForward()
         {
-            
+            List<int> testtape = new List<int>();
+            testtape.Add(0);
+            int testtapeHead = 0;
+
+            BrainMessInterpreter obj = new BrainMessInterpreter() { tape = testtape, tapeHead = testtapeHead };
+            obj.Forward();
+            Assert.AreEqual(testtape.Count, 2);
         }
         [TestMethod]
         public void TestMethodBackward()
         {
+            List<int> testtape = new List<int>();
+            testtape.Add(0);
+            int testtapeHead = 0;
 
+            BrainMessInterpreter obj = new BrainMessInterpreter() { tape = testtape, tapeHead = testtapeHead };
+            obj.Backward();
+            Assert.AreEqual(testtape.Count, 2);
         }
         [TestMethod]
         public void TestMethodIncrement()
         {
-             
-           BrainMessInterpreter obj= new BrainMessInterpreter(){tape =new List<int>(0),tapeHead = 0};
-            obj.Increment();
+             List<int> testtape= new List<int>();
+            testtape.Add(0);
+            int testtapeHead = 0;
 
-            Assert.Equals( obj.tape[0], 1);
+           BrainMessInterpreter obj= new BrainMessInterpreter(){tape = testtape,tapeHead = testtapeHead};
+            obj.Increment();
+            Assert.AreEqual( testtape[0],1);
         }
         [TestMethod]
         public void TestMethodDecrement()
         {
+            List<int> testtape = new List<int>();
+            testtape.Add(1);
+            int testtapeHead = 0;
 
+            BrainMessInterpreter obj = new BrainMessInterpreter() { tape = testtape, tapeHead = testtapeHead };
+            obj.Decrement();
+            Assert.AreEqual(testtape[0], 0);
         }
         [TestMethod]
         public void TestMethodLoopForward()
