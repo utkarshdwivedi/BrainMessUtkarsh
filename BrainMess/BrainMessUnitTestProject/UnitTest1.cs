28f1a94f91538829bde0c417b3772e71e7795a45
Utkarsh <utkarsh.dwivedi@jci.com>
2013-03-26 17:06:32 +0530

    checkin changes

--- BrainMess/BrainMessUnitTestProject/UnitTest1.cs
+++ BrainMess/BrainMessUnitTestProject/UnitTest1.cs
@@ -12,44 +12,52 @@ namespace BrainMessUnitTestProject
         [TestMethod]
         public void TestMethodForward()
         {
-            List<int> testtape = new List<int>();
-            testtape.Add(0);
-            int testtapeHead = 0;
+            List<int> testtape = new List<int>
+                {
+                    0
+                };
+            const int testtapeHead = 0;
 
-            BrainMessInterpreter obj = new BrainMessInterpreter() { tape = testtape, tapeHead = testtapeHead };
+            BrainMessInterpreter obj = new BrainMessInterpreter { tape = testtape, tapeHead = testtapeHead };
             obj.Forward();
             Assert.AreEqual(testtape.Count, 2);
         }
         [TestMethod]
         public void TestMethodBackward()
         {
-            List<int> testtape = new List<int>();
-            testtape.Add(0);
-            int testtapeHead = 0;
+            List<int> testtape = new List<int>
+                {
+                    0
+                };
+            
 
-            BrainMessInterpreter obj = new BrainMessInterpreter() { tape = testtape, tapeHead = testtapeHead };
+            BrainMessInterpreter obj = new BrainMessInterpreter { tape = testtape, tapeHead =1 };
             obj.Backward();
-            Assert.AreEqual(testtape.Count, 2);
+            Assert.AreEqual(obj.tapeHead, 0);
         }
         [TestMethod]
         public void TestMethodIncrement()
         {
-             List<int> testtape= new List<int>();
-            testtape.Add(0);
-            int testtapeHead = 0;
+             List<int> testtape= new List<int>
+                 {
+                     0
+                 };
+            const int testtapeHead = 0;
 
-           BrainMessInterpreter obj= new BrainMessInterpreter(){tape = testtape,tapeHead = testtapeHead};
+           BrainMessInterpreter obj= new BrainMessInterpreter {tape = testtape,tapeHead = testtapeHead};
             obj.Increment();
             Assert.AreEqual( testtape[0],1);
         }
         [TestMethod]
         public void TestMethodDecrement()
         {
-            List<int> testtape = new List<int>();
-            testtape.Add(1);
-            int testtapeHead = 0;
+            List<int> testtape = new List<int>
+                {
+                    1
+                };
+            const int testtapeHead = 0;
 
-            BrainMessInterpreter obj = new BrainMessInterpreter() { tape = testtape, tapeHead = testtapeHead };
+            BrainMessInterpreter obj = new BrainMessInterpreter { tape = testtape, tapeHead = testtapeHead };
             obj.Decrement();
             Assert.AreEqual(testtape[0], 0);
         }
